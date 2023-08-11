61c76f09bbc7180382c961a6ac24993796578d1c
MatheusSantanaDev <133030422+MatheusSantanaDev@users.noreply.github.com>
2023-08-11 19:59:19 -0300

    Update main.py
    implementacao de usuarios com flask.

--- api-flask-bd/venv/main.py
+++ api-flask-bd/venv/main.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from positions import data
+
 
 app = Flask(__name__)
+
+@app.rout('/positions', methods = ['GET'])
+def get_positions():
+    return make_response(
+        jsonify(data)
+    )
 
 app.run()
