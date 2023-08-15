4becedda0523158d8feb90a2a682315fae7c4a62
MatheusSantanaDev <matheus.santanadearaujo@outlook.com>
2023-08-15 19:23:43 -0300

    Login e autenticacao de usuarios
    Implementacao da API rest para login de usuarios em bd MySQL, todas as aplicacoes containerizadas

---
+++ api-flask-bd/venv/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin@localhost/Users'
+app.config['SECRET_KEY'] = 'secret'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
