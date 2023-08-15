692047dfd1abdbc7e82c0c9fadadc345a65259c1
MatheusSantanaDev <matheus.santanadearaujo@outlook.com>
2023-08-15 18:37:36 -0300

    Login e autenticacao de usuarios
    Implementacao da API rest para login de usuarios em bd MySQL, todas as aplicacoes containerizadas

--- api-flask-bd/venv/main.py
+++ api-flask-bd/venv/main.py
@@ -1,13 +1,46 @@
-from flask import Flask, make_response, jsonify
-from positions import data
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
+from app import app, db
+from app.models import User
+
+##limpa cache
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        pwd = request.form['password']
+
+        user = User(name, email, pwd)
+        db.session.add(user)
+        db.session.commit()
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        pwd = request.form['password']
+        
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.verify_password(pwd):
+            login_user(user)
+            return redirect(url_for('home'))
+
+    return render_template('login.html')
 
 
-app = Flask(__name__)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
-@app.rout('/positions', methods = ['GET'])
-def get_positions():
-    return make_response(
-        jsonify(data)
-    )
-
-app.run()
+app.run(debug=True)
